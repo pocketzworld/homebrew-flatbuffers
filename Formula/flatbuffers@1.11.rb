770234adfb663fe7101f44f7aad37dd20e62866d
Tin Tvrtković <tintvrtkovic@Tins-MacBook-Pro-2.local>
2020-11-11 22:49:38 +0100

    Initial formula import

---
+++ Formula/flatbuffers@1.11.rb
@@ -0,0 +1,63 @@
+class FlatbuffersAT111 < Formula
+  desc "Serialization library for C++, supporting Java, C#, and Go"
+  homepage "https://google.github.io/flatbuffers"
+  url "https://github.com/google/flatbuffers/archive/v1.11.0.tar.gz"
+  sha256 "3f4a286642094f45b1b77228656fbd7ea123964f19502f9ecfd29933fd23a50b"
+  head "https://github.com/google/flatbuffers.git"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", "-G", "Unix Makefiles", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    def testfbs; <<~EOS
+      // example IDL file
+
+      namespace MyGame.Sample;
+
+      enum Color:byte { Red = 0, Green, Blue = 2 }
+
+      union Any { Monster }  // add more elements..
+
+        struct Vec3 {
+          x:float;
+          y:float;
+          z:float;
+        }
+
+        table Monster {
+          pos:Vec3;
+          mana:short = 150;
+          hp:short = 100;
+          name:string;
+          friendly:bool = false (deprecated);
+          inventory:[ubyte];
+          color:Color = Blue;
+        }
+
+      root_type Monster;
+
+    EOS
+    end
+    (testpath/"test.fbs").write(testfbs)
+
+    def testjson; <<~EOS
+      {
+        pos: {
+          x: 1,
+          y: 2,
+          z: 3
+        },
+        hp: 80,
+        name: "MyMonster"
+      }
+    EOS
+    end
+    (testpath/"test.json").write(testjson)
+
+    system bin/"flatc", "-c", "-b", "test.fbs", "test.json"
+  end
+end
